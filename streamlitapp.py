d864f6d90f4fc105a800b9b3a3921df3e8df5d22
jeffreyzhang10 <jeffy20222@gmail.com>
2025-12-11 15:14:46 -0500

    add tab4 streamlit pcr

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,6 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from app_functions import build_model, fit_elastic_net, get_elasticnet_coefficients
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import plotly.express as px
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LinearRegression
+
 
 data = pd.read_csv("input_data_clean.csv")
 
@@ -36,6 +46,8 @@ with tab3:
     st.write("Add your EDA visualizations here.")
 
 with tab4:
+
+
     st.header("PCA / KMeans / PCR")
 
     st.subheader("Original PCA / K-Means Plot With 8 Clusters")
@@ -45,6 +57,74 @@ with tab4:
     st.subheader("PCA / K-Means Plot With 4 Clusters")
     st.write("Based on the elbow and silhouette methods, our group determined that 4 clusters was the most appropriate number, which can be shown below.")
     st.image("kmeans4.png", caption="K-Means with 8 Clusters (Initial)")
+
+    st.subheader("Principal Component Regression (PCR)")
+
+    num_components = st.slider(
+        "Select number of PCA components Here!",
+        min_value=1,
+        max_value=8, # len features, only 8 features later on 
+        value=2, 
+        step=1
+    )
+
+    st.write(f"Using **{num_components}** principal components for PCR...")
+
+    data = pd.read_csv("input_data_clean.csv")
+
+    # interested in offense only data
+    offense = data [data ['player_side'] == 'Offense'].copy()
+    # get pre snap data only
+    pre_snap = offense.loc[offense.groupby(['game_id', 'play_id', 'nfl_id'])['frame_id'].idxmin()]
+
+    features = ['prev_x', 'prev_y', 'o_clean', 'dir_radians', 's_clean', 'a_clean', 'v_x', 'v_y']
+    X = offense[features].values
+    y = offense['x_clean'].values
+
+    X_train, X_test, y_train, y_test = train_test_split( # Standard train-test split
+    X, y, test_size=0.25, random_state=42)
+
+
+    pcr_pipe = Pipeline([ # FULL PCR pipeline
+    ("scaler", StandardScaler()),
+    ("pca", PCA(n_components = num_components)), # changed here 
+    ("linreg", LinearRegression())])
+
+    pcr_pipe.fit(X_train, y_train) # fitting the PCR model on the training data
+
+    train_r2 = pcr_pipe.score(X_train, y_train)
+    test_r2 = pcr_pipe.score(X_test, y_test)
+
+    y_train_pred = pcr_pipe.predict(X_train)
+    y_test_pred = pcr_pipe.predict(X_test)
+
+    train_rmse = np.sqrt(np.mean((y_train - y_train_pred)**2))
+    test_rmse = np.sqrt(np.mean((y_test - y_test_pred)**2))
+
+    # Model Performance 
+
+    st.subheader("PCR Performance Metrics")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Train R²", f"{train_r2:.3f}")
+        st.metric("Train RMSE", f"{train_rmse:.2f}")
+
+    with col2:
+        st.metric("Test R²", f"{test_r2:.3f}")
+        st.metric("Test RMSE", f"{test_rmse:.2f}")
+
+    # Exploring Variance
+    pca_model = pcr_pipe.named_steps["pca"]
+
+    st.subheader("Explained Variance")
+    st.write(f"Total variance explained: **{pca_model.explained_variance_ratio_.sum():.3f}**")
+
+    pcr_df = pd.DataFrame({ # table the results 
+        "PC #": np.arange(1, num_components + 1),
+        "PCR Variance Explained": pca_model.explained_variance_ratio_
+    })
+    st.dataframe(pcr_df, width='stretch') # widen to the entire length of the app 
 
 
 with tab5:
